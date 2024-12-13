dac9f9a7dc2942d8dc0818a9bbfe3604ed3f710f
username000101 <cxx.username000101@gmail.com>
2024-12-14 00:16:59 +0300

    Use cast and parse template functions for type parsing; Change function signatures (remove the pqxx::connection& parameter); Add enum AddUserResult for more detailed results of the addUser function

--- include/PSQLWrapper.hpp
+++ include/PSQLWrapper.hpp
@@ -1,36 +1,49 @@
 #pragma once
 
 #include <csignal>
-#include <memory>
 #include <variant>
 
+#include <nlohmann/json.hpp>
 #include <pqxx/pqxx>
-#include <spdlog/spdlog.h>
 
 #include <User.hpp>
 
-void initDB(const std::string& name, std::shared_ptr<pqxx::connection> db);
-void addUser(User& user);
-void removeUser(std::variant<std::int64_t, User&> identificator, pqxx::connection& db);
-User getUser(std::variant<std::uint64_t, User&> identificator, pqxx::connection& db);
-void updateUser(User& user, pqxx::connection& db);
+enum AddUserResult {
+    OK, /* The user succefully added */
+    ALREADY_EXISTS, /* The user already exists */
+    BANNED, /* The user has been banned */
+    DB_ERROR, /* An error occurred on the database side */
+};
+
+AddUserResult addUser(User& user);
+void removeUser(std::variant<std::int64_t, User> identificator);
+User getUser(std::variant<std::uint64_t, User> identificator);
+void updateUser(User& user);
 
 namespace parsers {
-    UserLanguage parseUserLanguage(int Integer);
-    UserCountry parseUserCountry(int Integer);
+    template <typename ValueType, bool useCast, typename CastType> std::vector<ValueType> parse(const std::string& json) {
+        std::vector<ValueType> result;
+        if (json.empty())
+            return result;
 
-    int castUserLanguage(UserLanguage& ul);
-    int castUserCountry(UserCountry& uc);
+        auto j = nlohmann::json::parse(json);
+        for (auto& element : j)
+            if (useCast)
+                result.emplace_back(static_cast<CastType>(element.get<ValueType>()));
+            else
+                result.emplace_back(element.get<ValueType>());
 
-    std::vector<std::string> parseJSONMediaVector(const std::string& jsonMV);
-    std::string castMediaVector(std::vector<std::string>& mv);
+        return result;
+    }
 
-    std::vector<std::uint16_t> parseJSONPreferredAges(const std::string& jsonPA);
-    std::string castPreferredAges(std::vector<std::uint16_t>& pa);
-
-    std::vector<UserLanguage> parseJSONPreferredLanguages(const std::string& jsonPL);
-    std::string castPreferredLanguages(std::vector<UserLanguage>& pl);
-
-    std::vector<UserCountry> parseJSONPreferredCountries(const std::string& jsonPC);
-    std::string castPreferredCountries(std::vector<UserCountry>& pc);
+    template <typename ValueType, bool useCast, typename CastType> std::string cast(std::vector<ValueType>& v) {
+        nlohmann::json j;
+        for (auto& element : v) {
+            if (useCast)
+                j.emplace_back(static_cast<CastType>(element));
+            else
+                j.emplace_back(element);
+        }
+        return j.dump();
+    }
 }
