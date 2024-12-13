af15edee3e83f403004bc407a97b9e60941eb2a8
username000101 <cxx.username000101@gmail.com>
2024-12-14 00:19:21 +0300

    Implement removeUser and updateUser

---
+++ src/removeUser.cpp
@@ -0,0 +1,20 @@
+#include <PSQLWrapper.hpp>
+
+#include <variant>
+
+#include <pqxx/pqxx>
+#include <spdlog/spdlog.h>
+
+#include <Definitions.hpp>
+
+void removeUser(std::variant<std::int64_t, User> identificator) {
+    std::int64_t id = (std::holds_alternative<std::int64_t>(identificator) ? std::get<std::int64_t>(identificator) : std::get<User>(identificator).id);
+    pqxx::work w(*db::database);
+    try {
+        w.exec("DELETE FROM users WHERE id = " + std::to_string(id) + ";");
+        w.commit();
+    } catch (std::exception& dberr) {
+        spdlog::get("PSQL")->log(spdlog::level::err, "Failed to remove user: pqxx created an exception: {}", dberr.what());
+    return;        }
+    spdlog::get("PSQL")->log(spdlog::level::info, "Deleted user with id {}", id);
+}
---
+++ src/updateUser.cpp
@@ -0,0 +1,30 @@
+#include <PSQLWrapper.hpp>
+
+#include <pqxx/pqxx>
+#include <spdlog/spdlog.h>
+
+#include <Definitions.hpp>
+
+void updateUser(User& user) {
+    pqxx::work w(*db::database);
+    try {
+        pqxx::result checkUser = w.exec("SELECT * FROM users WHERE id = " + std::to_string(user.id) + ";");
+        if (checkUser.empty()) {
+            spdlog::get("PSQL")->log(spdlog::level::warn, "Failed to update user: user with id {} was not found (use the addUser to add user)", user.id);
+            return;
+        }
+
+        w.exec("UPDATE users "
+               "SET tgFirstName = " + w.esc(user.tgFirstName) +
+               ", tgLastName = " + w.esc(user.tgLastName) +
+               ", age = " + std::to_string(user.age) +
+               ", name = " + w.esc(user.name) +
+               ", bio = " + w.esc(user.bio) +
+               " " +
+               "WHERE id = " + std::to_string(user.id) + ";");
+
+        w.commit();
+    } catch (std::exception& dberr) {
+        spdlog::get("PSQL")->log(spdlog::level::err, "Failed to update user: pqxx created an exception: {}", dberr.what());
+    }
+}
